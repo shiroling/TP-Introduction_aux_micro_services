863ee3de546280378494820ca5d10502b5d2c3a3
Benito65536 <little.benoit@gmail.com>
2023-03-10 17:33:32 +0100

    final it's working

--- api_entite_micro_service/app.py
+++ api_entite_micro_service/app.py
@@ -8,7 +8,7 @@ api = Api(app, version='0.1', title='API Python', description="Cette API permet 
 namespace_entity = api.namespace('entite', description="Contient une liste d'opérations sur les entités.")
 entity_model = api.model('Entité', {
 	'id': fields. Integer (readonly=True, description="Identifiant unique de l'entité", example=1),
-	'libelle_entité': fields.String(required=True, description="Le libellé de l'entité", example='Ma LUBULULE'), 
+	'libellé_entité': fields.String(required=True, description="Le libellé de l'entité", example='Ma LUBULULE'), 
 	'contenu_entité': fields.String(required=True, description="Le conteny de l'entité", example='Mon contenu')
 })
 
@@ -20,7 +20,7 @@ class EntityIdQueries(Resource):
 	@namespace_entity.response (400, 'Fail')
 	def post(self, id):
 		print(f"Réception d'une requête d'ajout d'entité id={id}")
-		if 'libelle_entité' not in api.payload or api.payload['libellé_entité'] == "":
+		if 'libellé_entité' not in api.payload or api.payload['libellé_entité'] == "":
 			return "Libellé non trouvé ou vide", 400
 		if 'contenu_entité' not in api.payload or api.payload['contenu_entité'] == "":
 			return "contenu non trouvé ou vide", 400
